fc4263a4fb2b31e96786f497957138bc14fba37a
Icaro Weiler <icarog28@gmail.com>
2026-06-24 04:47:08 -0300

    feature: cria validacao com zod e integracao com login

--- cinedash/src/features/auth/ui/form.tsx
+++ cinedash/src/features/auth/ui/form.tsx
@@ -4,6 +4,7 @@ import { Input } from '#/shared/components/ui/input'
 import { useState } from 'react'
 import { generateBrowserToken, logInUser } from '../lib/login'
 import type { LoginFormData } from '../model/types/form'
+import { loginSchema } from '../model/schema/loginForm'
 
 export function LoginForm() {
   const [formState, setFormState] = useState<LoginFormData>({
@@ -11,7 +12,25 @@ export function LoginForm() {
     password: '',
   })
 
+  const [errors, setErrors] = useState<
+    Partial<Record<keyof LoginFormData, string>>
+  >({})
+
   const handleLogin = () => {
+    const result = loginSchema.safeParse(formState)
+
+    if (!result.success) {
+      const fieldErrors = result.error.flatten().fieldErrors
+
+      setErrors({
+        email: fieldErrors.email?.[0],
+        password: fieldErrors.password?.[0],
+      })
+      return
+    }
+
+    setErrors({})
+
     const token = generateBrowserToken(formState)
     logInUser(token)
   }
@@ -24,6 +43,11 @@ export function LoginForm() {
         value={formState.email}
         onChange={(e) => setFormState({ ...formState, email: e.target.value })}
       />
+
+      {errors.email && (
+        <span className="text-sm text-red-500">{errors.email}</span>
+      )}
+
       <Input
         type="password"
         placeholder="Senha"
@@ -32,6 +56,10 @@ export function LoginForm() {
           setFormState({ ...formState, password: e.target.value })
         }
       />
+
+      {errors.password && (
+        <span className="text-sm text-red-500">{errors.password}</span>
+      )}
 
       <div className="flex gap-2 mt-3 items-center justify-center">
         <Button onClick={handleLogin} className="p-2">
