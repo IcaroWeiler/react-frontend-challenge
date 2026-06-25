6e291b9a4399930c1930401388e64ce405e0781d
Icaro Weiler <icarog28@gmail.com>
2026-06-25 15:22:58 -0300

    feature: troca select no filtro por combobox multiplo

--- cinedash/src/features/discovery/ui/filters.tsx
+++ cinedash/src/features/discovery/ui/filters.tsx
@@ -1,13 +1,13 @@
 import { Input } from '#/shared/components/ui/input'
 import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from '#/shared/components/ui/select'
+  Combobox,
+  ComboboxChip,
+  ComboboxChips,
+  ComboboxChipsInput,
+  ComboboxContent,
+  ComboboxItem,
+  ComboboxList,
+} from '#/shared/components/ui/combobox'
 import { useEffect, useState } from 'react'
 import { genres } from '../models/genres/genres'
 import { DatePicker } from '#/shared/components/ui/datepicker'
@@ -15,15 +15,17 @@ import type { Filter } from '../models/types/filter'
 
 export const Filters = () => {
   const [filter, setFilter] = useState<Partial<Filter>>({})
+  const [selectedGenres, setSelectedGenres] = useState<string[]>([])
 
   const ONLY_NUMBERS_REGEX = /^\d*$/
 
-  const handleGenreChange = (value: string) => {
-    setFilter({ ...filter, with_genres: value })
+  const handleGenreChange = (value: string[]) => {
+    setSelectedGenres(value)
+    setFilter((prev) => ({ ...prev, with_genres: value.join(',') }))
   }
 
   const handleMinRatingChange = (value: string) => {
-    setFilter({ ...filter, 'vote_average.gte': value })
+    setFilter((prev) => ({ ...prev, 'vote_average.gte': value }))
   }
 
   useEffect(() => {
@@ -32,21 +34,33 @@ export const Filters = () => {
 
   return (
     <div className="flex gap-4">
-      <Select onValueChange={handleGenreChange}>
-        <SelectTrigger>
-          <SelectValue placeholder="Select a genre" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>Genres</SelectLabel>
+      <Combobox
+        multiple
+        value={selectedGenres}
+        onValueChange={handleGenreChange}
+      >
+        <ComboboxChips className="min-w-56">
+          {selectedGenres.map((genreId) => {
+            const genre = genres.find((item) => item.id.toString() === genreId)
+
+            return (
+              <ComboboxChip key={genreId}>
+                {genre?.name ?? genreId}
+              </ComboboxChip>
+            )
+          })}
+          <ComboboxChipsInput placeholder="Select genres" />
+        </ComboboxChips>
+        <ComboboxContent>
+          <ComboboxList>
             {genres.map((genre) => (
-              <SelectItem key={genre.id} value={genre.id.toString()}>
+              <ComboboxItem key={genre.id} value={genre.id.toString()}>
                 {genre.name}
-              </SelectItem>
+              </ComboboxItem>
             ))}
-          </SelectGroup>
-        </SelectContent>
-      </Select>
+          </ComboboxList>
+        </ComboboxContent>
+      </Combobox>
 
       <Input
         value={filter['vote_average.gte']}
